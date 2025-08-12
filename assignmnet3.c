9d983280549ac89087d4a41c52ce863dc1c244e2
Sridwip-mandal <sridwipmandal@gmail.com>
2025-08-12 13:44:29 +0530

    Add files via upload

---
+++ assignmnet3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	float a,b, diff;
+printf("enter a number:");
+scanf("%f",&a);
+printf("Enter a Number:");
+scanf("%f",&b);
+diff=a-b;
+printf("diff is:%f",diff);
+return 0;
+}
